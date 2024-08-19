51ceb31e392b95595f531a8cd9ad594c7104f3e0
MaliQoc <alihasanozankoc@gmail.com>
2024-08-19 18:28:07 +0300

    product

--- edit_product/main.go
+++ edit_product/main.go
@@ -3,15 +3,42 @@ package main
 import (
 	"fmt"
 	"golesson/project"
+	"log"
 )
 
 func main() {
-	/* product, _ := project.AddProduct()
-	fmt.Println(product) */
 
-	products, _ := project.GetAllProducts()
-
-	for i := 0; i < len(products); i++ {
-		fmt.Println(products[i])
+	products, err := project.GetAllProducts()
+	if err != nil {
+		log.Fatalf("Ürünler alınamadı: %v", err)
 	}
+
+	fmt.Println("Tüm Ürünler:")
+	for i := 0; i < len(products); i++ {
+		fmt.Printf("ID: %d, Ürün Adı: %s, Kategori ID: %d, Fiyat: %.2f\n", products[i].Id, products[i].ProductName, products[i].CategoryId, products[i].UnitPrice)
+	}
+
+	/*
+		product, err := project.AddProduct()
+		if err != nil {
+			log.Fatalf("Ürün eklenemedi: %v", err)
+		}
+		fmt.Printf("\nYeni eklenen ürün: ID: %d, Ürün Adı: %s, Kategori ID: %d, Fiyat: %.2f\n", product.Id, product.ProductName, product.CategoryId, product.UnitPrice)
+
+
+		updatedProduct := project.Product{ProductName: "Updated Telephone", CategoryId: 1, UnitPrice: 5500.0}
+		updatedProductResponse, err := project.UpdateProduct(product.Id, updatedProduct)
+		if err != nil {
+			log.Fatalf("Ürün güncellenemedi: %v", err)
+		}
+		fmt.Printf("\nGüncellenen ürün: ID: %d, Ürün Adı: %s, Kategori ID: %d, Fiyat: %.2f\n", updatedProductResponse.Id, updatedProductResponse.ProductName, updatedProductResponse.CategoryId, updatedProductResponse.UnitPrice)
+
+
+		err = project.DeleteProduct(product.Id)
+		if err != nil {
+			log.Fatalf("Ürün silinemedi: %v", err)
+		} else {
+			fmt.Printf("\nÜrün başarıyla silindi: ID: %d\n", product.Id)
+		}
+	*/
 }
--- edit_product/project/edit_product.go
+++ edit_product/project/edit_product.go
@@ -3,30 +3,28 @@ package project
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
 type Product struct {
-	Id          int     `json: "id"`
-	ProductName string  `json: "productName"`
-	CategoryId  int     `json: "categoryId"`
-	UnitPrice   float64 `json: "unitPrice"`
+	Id          int     `json:"id"`
+	ProductName string  `json:"productName"`
+	CategoryId  int     `json:"categoryId"`
+	UnitPrice   float64 `json:"unitPrice"`
 }
 
 type Category struct {
-	Id           int    `json: "id"`
-	CategoryName string `json: "categoryName"`
+	Id           int    `json:"id"`
+	CategoryName string `json:"categoryName"`
 }
 
 func GetAllProducts() ([]Product, error) {
-
 	response, err := http.Get("http://localhost:3000/products")
-
 	if err != nil {
 		return nil, err
 	}
-
 	defer response.Body.Close()
 
 	bodyBytes, _ := ioutil.ReadAll(response.Body)
@@ -37,22 +35,67 @@ func GetAllProducts() ([]Product, error) {
 }
 
 func AddProduct() (Product, error) {
-
 	product := Product{ProductName: "Telephone", CategoryId: 1, UnitPrice: 6000.0}
 	jsonProduct, err := json.Marshal(product)
-
-	response, err := http.Post("http://localhost:3000/products", "application/json;charset=utf-8", bytes.NewBuffer(jsonProduct))
-
 	if err != nil {
 		return Product{}, err
 	}
 
+	response, err := http.Post("http://localhost:3000/products", "application/json;charset=utf-8", bytes.NewBuffer(jsonProduct))
+	if err != nil {
+		return Product{}, err
+	}
 	defer response.Body.Close()
 
 	bodyBytes, _ := ioutil.ReadAll(response.Body)
 
 	var productResponse Product
-
 	json.Unmarshal(bodyBytes, &productResponse)
 	return productResponse, nil
+}
+
+func UpdateProduct(productId int, updatedProduct Product) (Product, error) {
+	jsonProduct, err := json.Marshal(updatedProduct)
+	if err != nil {
+		return Product{}, err
+	}
+
+	request, err := http.NewRequest(http.MethodPut, fmt.Sprintf("http://localhost:3000/products/%d", productId), bytes.NewBuffer(jsonProduct))
+	if err != nil {
+		return Product{}, err
+	}
+	request.Header.Set("Content-Type", "application/json;charset=utf-8")
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		return Product{}, err
+	}
+	defer response.Body.Close()
+
+	bodyBytes, _ := ioutil.ReadAll(response.Body)
+
+	var productResponse Product
+	json.Unmarshal(bodyBytes, &productResponse)
+	return productResponse, nil
+}
+
+func DeleteProduct(productId int) error {
+	request, err := http.NewRequest(http.MethodDelete, fmt.Sprintf("http://localhost:3000/products/%d", productId), nil)
+	if err != nil {
+		return err
+	}
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to delete product, status code: %d", response.StatusCode)
+	}
+
+	return nil
 }
